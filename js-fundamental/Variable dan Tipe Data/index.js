ebae3b18d29ce920eda2b886065faafbab24d147
Levendigheid <jordanhiskia721@gmail.com>
2023-06-18 14:04:40 +0700

    Menambahkan Variable dan Tipe Data

---
+++ js-fundamental/Variable dan Tipe Data/index.js
@@ -0,0 +1,52 @@
+// Variable dan Tipe Data
+
+// console.log("Hello World.");        
+
+// Study Case
+// Buatlah algoritma dan pseudocode dan codingan dari keliling persegi panjang.
+
+/** 
+ * ALGORITMA
+ * 1. Tentukan Variable Panjang
+ * 2. Tentukan Variable Lebar
+ * 3. Tentukan Variable Keliling
+ * 4. Isi keliling dengan rumus 2 * Panjang + 2 * Lebar
+ * 5. Cetaik keliling persegi panjang
+ * 
+ * PSEUDOCODE
+ * 1. Store Panjang with P
+ * 2. Store Lebar with L
+ * 3. STORE KELILING WITH MUL Panjang ADD 2 MUL lebar
+ * 
+ */
+
+// var panjang = 2;
+// var lebar = 2;
+// var keliling = 2 * panjang + 2 * lebar;
+// console.log(keliling);
+
+//Study Case
+
+// Tentukan algoritma dan pseudocode dan codingan dari luas permukaan tabung.
+
+/**
+ * ALGOROTMA 
+ * 1. Tentukan variable pi
+ * 2. Tentukan variable r
+ * 3. Tentukan variable t
+ * 4. Isi luas permukaan tabung dengan 2 * pi * r (r+t)
+ * 5. Cetak luas permukaan tabung 
+ * 
+ * PSEUDOCODE 
+ * 1. Store pi with P
+ * 2. Store r with R
+ * 3. Store t with T
+ * 4. Store LUAS with 2 MUL pi MUL r MUL (r+t)
+ * 
+ *
+ */
+var pi = 2;
+var r = 5;
+var t = 7;
+var luas = 2 * pi * r * (r + t);
+console.log(luas);
